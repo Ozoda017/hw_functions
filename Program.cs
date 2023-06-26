a813ee7ad99d6c9cb9aea5848aa7b745b88bbd79
Ozoda <ikramova.ozoda@mail.ru>
2023-06-27 00:12:54 +0500

    YES

--- Program.cs
+++ Program.cs
@@ -19,3 +19,29 @@ int Exponentiation(int A, int B){
 
   int exponentiation = Exponentiation(A, B);
   Console.WriteLine("А в сепени B: " + exponentiation);
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ int SumNumber(int number){
+
+      int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+
+  for (int i = 0; i < counter; i++){
+      advance = number - number % 10;
+      result = result + (number - advance);
+      number = number / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(number);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
