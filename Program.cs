6179acec4d9c3bf7cc147c1c549161c0e1063c79
Ozoda <ikramova.ozoda@mail.ru>
2023-06-27 01:03:32 +0500

    YESSS

--- Program.cs
+++ Program.cs
@@ -45,3 +45,21 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int sumNumber = SumNumber(number);
 Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (можно указать любой промежуток)
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int[] getArray(int size)
+{
+    int[] result = new int[size]; 
+for (int i = 0; i < size; i++)
+{
+    result[i] = new Random().Next(2,19);
+}
+return result;
+}
+
+int[] array = getArray(8);
+Console.WriteLine($"[{String.Join(",",array)}]");
